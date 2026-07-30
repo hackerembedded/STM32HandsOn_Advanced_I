fda2a810965ffb0b62d362c8bc8c392c2b6f23a6
Bruno Montanari <bruno.montanari@st.com>
2026-07-30 00:56:12 -0300

    added NanoEdge
    NanoEdge example 3 classes

---
+++ LabAINanoEdge/STM32CubeIDE/Debug/Drivers/BSP/Components/lsm6dsl.d
@@ -0,0 +1,6 @@
+Drivers/BSP/Components/lsm6dsl.o: \
+ C:/Users/montanab/Insper/LabAIHAR_datalog/Drivers/BSP/Components/lsm6dsl/lsm6dsl.c \
+ C:/Users/montanab/Insper/LabAIHAR_datalog/Drivers/BSP/Components/lsm6dsl/lsm6dsl.h \
+ C:/Users/montanab/Insper/LabAIHAR_datalog/Drivers/BSP/Components/lsm6dsl/lsm6dsl_reg.h
+C:/Users/montanab/Insper/LabAIHAR_datalog/Drivers/BSP/Components/lsm6dsl/lsm6dsl.h:
+C:/Users/montanab/Insper/LabAIHAR_datalog/Drivers/BSP/Components/lsm6dsl/lsm6dsl_reg.h:
